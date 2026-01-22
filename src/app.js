f87a3138490d0499a3429eab7cc4d175e3285f0a
Muthu kumaran <muthumuthu7699@gmail.com>
2026-01-22 19:07:23 +0600

    bug fixes

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const cors = require("cors")
 const app = express();
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: ["http://localhost:5173", "https://expense-tracker-fe-phi.vercel.app/"],
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type"],
     })
